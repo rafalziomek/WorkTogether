a7ee12464df2433f8f0c9f74dc06522c177b8511
rziomek <rziomek@LAPTOP-J6URE51E>
2016-10-30 21:16:27 +0100

    Add first models class and chagne AppUser name to User

---
+++ WorkTogether/Models/TimeTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorkTogether.Models
+{
+    public class TimeTable
+    {
+        [Display(Name = "Id:")]
+        public int Id { get; set; }
+    }
+}
